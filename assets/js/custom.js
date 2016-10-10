ac7ad1b4e513b14f7c874a6e767eac67ee197424
Akshada Deshmukh <akshada.deshmukh@gslab.com>
2016-10-10 11:07:36 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,6 +7,11 @@
 		});  	
 		jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:false,overlay_gallery: false,social_tools:false,deeplinking:false});
 	}); 
-
+// contributors_img
+	function update()
+	{
+		document.getElementById("img-responsive ").style.clip=rect(10px, 290px, 190px, 10px);
+	}
+	
 		
 })(jQuery);
